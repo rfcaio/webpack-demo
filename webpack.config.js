bdd1f01e9bf722b98b68879235dcf6f89815e90a
Caio Rodrigues <rfcaio90@gmail.com>
2019-05-16 13:31:30 -0300

    developing-chapter-02

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+const webpack = require('webpack')
+
+let devServer = {
+  host: process.env.HOST,
+  open: true,
+  overlay: true,
+  port: process.env.PORT,
+  stats: 'errors-only'
+}
+
+let plugins = [
+  new HtmlWebpackPlugin({
+    title: 'Webpack demo'
+  })
+]
+
+// https://survivejs.com/webpack/developing/webpack-dev-server/#polling-instead-of-watching-files
+if (process.env.MODE === 'poll') {
+  const watchIgnorePlugin = new webpack.WatchIgnorePlugin([
+    path.join(__dirname, 'node_modules')
+  ])
+
+  const watchOptions = {
+    aggregateTimeout: 5000,
+    poll: 1000
+  }
+
+  devServer = Object.assign({}, devServer, { watchOptions })
+  plugins = [...plugins, watchIgnorePlugin]
+}
 
 module.exports = {
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Webpack demo'
-    })
-  ]
+  devServer,
+  plugins
 }
