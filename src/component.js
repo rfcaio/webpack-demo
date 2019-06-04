d6421282339fe451f95d5e8282b7854c09f02c9d
Caio Rodrigues <rfcaio90@gmail.com>
2019-06-04 13:43:04 -0300

    building-chapter-03

--- src/component.js
+++ src/component.js
@@ -3,5 +3,14 @@ export default (text = 'Hello, world.') => {
   const element = document.createElement('div')
   element.className = 'pure-button'
   element.textContent = text
+  element.onclick = () => {
+    import('./lazy')
+      .then(lazy => {
+        element.textContent = lazy.default
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
   return element
 }
